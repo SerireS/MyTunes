a7272740e490c8a6c9aa390e4b2509a767af636f
FimFi <55874743+FimFi@users.noreply.github.com>
2019-11-25 10:39:27 +0100

    yes

--- Zpotify/src/zpotify/dal/SongDAO.java
+++ Zpotify/src/zpotify/dal/SongDAO.java
@@ -5,6 +5,7 @@
  */
 package zpotify.dal;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import zpotify.be.Song;
@@ -13,11 +14,23 @@ import zpotify.be.Song;
  *
  * @author Jonas
  */
-public class SongDAO
-{
-    public List<Song> getAllSongs()
-    {
+public class SongDAO {
+
+    public List<Song> getAllSongs() {
         List<Song> allSongs = new ArrayList<>();
         return allSongs;
     }
+
+
+public void deleteSong(Song song) throws IOException
+{
+List<Song> allSongs = getAllSongs();
+
 }
+
+public void updateSong(Song song)
+{
+    List<Song> allSongs = getAllSongs();
+}
+}
+

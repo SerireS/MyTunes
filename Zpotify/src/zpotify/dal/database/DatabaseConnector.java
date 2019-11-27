a775680e89cc26ffe32c005a642448175a7e92b8
The Joker <54842770+Nbruunj@users.noreply.github.com>
2019-11-27 11:21:16 +0100

    songDBDAO

--- Zpotify/src/zpotify/dal/database/DatabaseConnector.java
+++ Zpotify/src/zpotify/dal/database/DatabaseConnector.java
@@ -5,10 +5,33 @@
  */
 package zpotify.dal.database;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
 /**
  *
  * @author nbruu
  */
-public class DatabaseConnector {
+public class DatabaseConnector 
+{
+    private SQLServerDataSource dataSource;
     
+    public DatabaseConnector() throws IOException 
+    {
+        Properties props = new Properties();
+        props.load(new FileReader("DBSettings.txt"));
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(props.getProperty("database"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));        
+        dataSource.setServerName(props.getProperty("server"));
+    }
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
 }
---
+++ Zpotify/src/zpotify/dal/database/SongDBDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zpotify.dal.database;
+
+import zpotify.dal.SongDAO;
+
+/**
+ *
+ * @author nbruu
+ */
+public class SongDBDAO implements SongDAO
+{
+     private DatabaseConnector dbCon;
+     
+     public SongDBDAO() throws Exception
+     {
+         dbCon = new DatabaseConnector();
+     }
+     
+     public static void main(String[] args)
+    {
+        try
+        {
+            SongDBDAO songDAO = new SongDBDAO();
+            
+            string TXt
+            
+        }
+        
+    }
+    
+}

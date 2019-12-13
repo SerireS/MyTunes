2ff7081817faa4bd657af76cb0bf2f367beb7824
FimFi <55874743+FimFi@users.noreply.github.com>
2019-12-13 11:19:16 +0100

    fungere

--- Zpotify/src/zpotify/dal/database/SongDBDAO.java
+++ Zpotify/src/zpotify/dal/database/SongDBDAO.java
@@ -92,11 +92,12 @@ public class SongDBDAO {
 
     public boolean updateSong(String title, int id) throws SQLServerException, SQLException {
         try ( Connection con = dbCon.getConnection()) {
-            String sql = "UPDATE Songs SET title = '?' WHERE songId = ?;";
+            String sql = "UPDATE Songs SET title = ? WHERE songId = ?;";
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             System.out.println(id);
-            ps.setInt(0, id);
+            System.out.println(title);
             ps.setString(1, title);
+            ps.setInt(2, id);
             ps.executeUpdate();
             System.out.println("It worked or atleast I think it does");
             return true;

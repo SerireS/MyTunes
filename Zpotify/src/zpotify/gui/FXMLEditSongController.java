e281952f28d8444bb6f1d12306501041ce9bcf16
FimFi <55874743+FimFi@users.noreply.github.com>
2019-12-13 10:48:40 +0100

    idk how to fix shit

--- Zpotify/src/zpotify/gui/FXMLEditSongController.java
+++ Zpotify/src/zpotify/gui/FXMLEditSongController.java
@@ -11,7 +11,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import zpotify.gui.model.SongModel;
 
 /**
  * FXML Controller class
@@ -19,11 +21,16 @@ import javafx.stage.Stage;
  * @author Peter
  */
 public class FXMLEditSongController implements Initializable {
+    
+    private SongModel songModel;
+    private FXMLDocumentController controller;
 
     @FXML
     private Button btn_cancel;
     @FXML
     private Button btn_save;
+    @FXML
+    private TextField txt_title;
 
     /**
      * Initializes the controller class.
@@ -32,6 +39,12 @@ public class FXMLEditSongController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    // Denne metode sikre vi har fat i den samme SongModel og Controller hele tiden.
+    void ApplyImportantData(SongModel songModel, FXMLDocumentController controller) {
+        this.songModel = songModel;
+        this.controller = controller;
+    }
 
     @FXML
     private void handleButtonActionCancel(ActionEvent event) 
@@ -42,6 +55,22 @@ public class FXMLEditSongController implements Initializable {
 
     @FXML
     private void handleButtonActionSave(ActionEvent event) {
+        String title = txt_title.getText().trim();
+//        String place = "musik/" + txt_MP3_File.getText().trim();
+        Stage stage = (Stage) btn_save.getScene().getWindow();
+       // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
+       // Det er først muligt at lukke dialogen ned når der er noget i feltet
+        if(title.length() == 0)
+        {
+            Border warning = new Border(new BorderStroke(Color.RED, 
+            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
+            
+            txt_title.setBorder(warning);
+        } else
+        {
+        this.songModel.updateSong(title, id);
+        stage.close();
+        }
     }
     
 }

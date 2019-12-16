7613f9bbee19227dc71181e9cd4ac2a62095dc22
SerireS <55481182+SerireS@users.noreply.github.com>
2019-12-16 10:40:56 +0100

    clean up

--- Zpotify/src/zpotify/gui/FXMLNewSongController.java
+++ Zpotify/src/zpotify/gui/FXMLNewSongController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import zpotify.dal.DalException;
@@ -19,12 +21,6 @@ import zpotify.gui.model.SongModel;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
@@ -35,7 +31,7 @@ public class FXMLNewSongController implements Initializable
 {
     private SongModel songModel;
     private FXMLDocumentController controller;
-            
+
     private String path;
 
     @FXML
@@ -56,43 +52,45 @@ public class FXMLNewSongController implements Initializable
     public void initialize(URL url, ResourceBundle rb)
     {
     }
+
     // Denne metode sikre vi har fat i den samme SongModel og Controller hele tiden.
-    void ApplyImportantData(SongModel songModel, FXMLDocumentController controller) {
+    void ApplyImportantData(SongModel songModel, FXMLDocumentController controller)
+    {
         this.songModel = songModel;
         this.controller = controller;
     }
 
     /*
-    * 
-    */
+     *
+     */
     @FXML
     private void handleButtonActionSave(ActionEvent event) throws DalException
-    {   
+    {
         String title = txt_title.getText().trim();
         String place = "musik/" + txt_MP3_File.getText().trim();
         Stage stage = (Stage) btn_save.getScene().getWindow();
-       // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
-       // Det er først muligt at lukke dialogen ned når der er noget i feltet
-        if(title.length() == 0)
+        // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
+        // Det er først muligt at lukke dialogen ned når der er noget i feltet
+        if (title.length() == 0)
         {
-            Border warning = new Border(new BorderStroke(Color.RED, 
-            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
-            
+            Border warning = new Border(new BorderStroke(Color.RED,
+                    BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
+
             txt_title.setBorder(warning);
         } else
         {
-        this.songModel.createSong(title, place);
-        stage.close();
+            this.songModel.createSong(title, place);
+            stage.close();
         }
     }
 
     @FXML
     private void handleButtonActionChoose(ActionEvent event)
 
-            // Chose button to new song function. Chooses a file limited to mp3 only and getting the path of the song.
+    // Chose button to new song function. Chooses a file limited to mp3 only and getting the path of the song.
     {
         FileChooser fc = new FileChooser();
-        FileChooser.ExtensionFilter onlyMP3 = new FileChooser.ExtensionFilter("Mp3 Files","*.mp3");
+        FileChooser.ExtensionFilter onlyMP3 = new FileChooser.ExtensionFilter("Mp3 Files", "*.mp3");
         fc.getExtensionFilters().add(onlyMP3);
         fc.setInitialDirectory(new File("musik"));
         File selectedFile = fc.showOpenDialog(null);
@@ -100,7 +98,7 @@ public class FXMLNewSongController implements Initializable
         if (selectedFile != null)
         {
             txt_MP3_File.setText(selectedFile.getName());
-            
+
         }
     }
 

f978e0ad0b52c9e07592f8d97f170d98fa956f86
SerireS <55481182+SerireS@users.noreply.github.com>
2019-12-16 10:40:34 +0100

    clean up

--- Zpotify/src/zpotify/gui/FXMLEditSongController.java
+++ Zpotify/src/zpotify/gui/FXMLEditSongController.java
@@ -5,32 +5,28 @@
  */
 package zpotify.gui;
 
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
-import javafx.css.CssParser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import zpotify.be.Song;
 import zpotify.gui.model.SongModel;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * FXML Controller class
  *
  * @author Peter
  */
-public class FXMLEditSongController extends FXMLDocumentController {
-    
+public class FXMLEditSongController extends FXMLDocumentController
+{
+
     private SongModel songModel;
     private FXMLDocumentController controller;
     private Song SelectedSong;
@@ -46,43 +42,46 @@ public class FXMLEditSongController extends FXMLDocumentController {
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         // TODO
-    }    
-    
+    }
+
     // Denne metode sikre vi har fat i den samme SongModel og Controller hele tiden.
-    void ApplyImportantData(SongModel songModel, FXMLDocumentController controller, Song selectedSong) {
-        
+    void ApplyImportantData(SongModel songModel, FXMLDocumentController controller, Song selectedSong)
+    {
+
         this.songModel = songModel;
         this.controller = controller;
         this.SelectedSong = selectedSong;
     }
 
     @FXML
-    private void handleButtonActionCancel(ActionEvent event) 
+    private void handleButtonActionCancel(ActionEvent event)
     {
         Stage stage = (Stage) btn_cancel.getScene().getWindow();
         stage.close();
     }
 
     @FXML
-    private void handleButtonActionSave(ActionEvent event) throws SQLException {
+    private void handleButtonActionSave(ActionEvent event) throws SQLException
+    {
         String title = txt_title.getText().trim();
         int id = this.SelectedSong.getId();
         Stage stage = (Stage) btn_save.getScene().getWindow();
-       // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
-       // Det er først muligt at lukke dialogen ned når der er noget i feltet
-        if(title.length() == 0)
+        // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
+        // Det er først muligt at lukke dialogen ned når der er noget i feltet
+        if (title.length() == 0)
         {
-            Border warning = new Border(new BorderStroke(Color.RED, 
-            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
-            
+            Border warning = new Border(new BorderStroke(Color.RED,
+                    BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
+
             txt_title.setBorder(warning);
         } else
         {
-        this.songModel.updateSong(title, id);
-        stage.close();
+            this.songModel.updateSong(title, id);
+            stage.close();
         }
     }
-    
+
 }

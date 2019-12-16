45082fc223b3ad91391b063a62587b5df21f786d
SerireS <55481182+SerireS@users.noreply.github.com>
2019-12-16 10:40:22 +0100

    clean up

--- Zpotify/src/zpotify/gui/FXMLEditPlaylistController.java
+++ Zpotify/src/zpotify/gui/FXMLEditPlaylistController.java
@@ -5,31 +5,29 @@
  */
 package zpotify.gui;
 
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import zpotify.be.Playlist;
 import zpotify.gui.model.PlaylistModel;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
  * FXML Controller class
  *
  * @author Peter
  */
-public class FXMLEditPlaylistController implements Initializable {
-    
+public class FXMLEditPlaylistController implements Initializable
+{
+
     private PlaylistModel playlistModel;
     private FXMLDocumentController controller;
     private Playlist selectedPlaylist;
@@ -45,39 +43,44 @@ public class FXMLEditPlaylistController implements Initializable {
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         // TODO
-    }    
-void ApplyImportantData(PlaylistModel playlistModel, FXMLDocumentController controller, Playlist selectedPlaylist) {
+    }
+
+    void ApplyImportantData(PlaylistModel playlistModel, FXMLDocumentController controller, Playlist selectedPlaylist)
+    {
         this.playlistModel = playlistModel;
         this.controller = controller;
         this.selectedPlaylist = selectedPlaylist;
     }
+
     @FXML
-    private void handleButtonActionCancel(ActionEvent event) 
+    private void handleButtonActionCancel(ActionEvent event)
     {
         Stage stage = (Stage) btn_cancel.getScene().getWindow();
         stage.close();
     }
 
     @FXML
-    private void handleButtonActionEdit(ActionEvent event) throws SQLException {
+    private void handleButtonActionEdit(ActionEvent event) throws SQLException
+    {
         String title = txt_playlistName.getText().trim();
         int id = this.selectedPlaylist.getPlaylistId();
         Stage stage = (Stage) btn_edit.getScene().getWindow();
-       // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
-       // Det er først muligt at lukke dialogen ned når der er noget i feltet
-        if(title.length() == 0)
+        // Tester om der er noget i title felt, sætter en rød border for at indikere her mangles noget
+        // Det er først muligt at lukke dialogen ned når der er noget i feltet
+        if (title.length() == 0)
         {
-            Border warning = new Border(new BorderStroke(Color.RED, 
-            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
-            
+            Border warning = new Border(new BorderStroke(Color.RED,
+                    BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
+
             txt_playlistName.setBorder(warning);
         } else
         {
-        this.playlistModel.updatePlaylist(title, id);
-        stage.close();
+            this.playlistModel.updatePlaylist(title, id);
+            stage.close();
         }
     }
-    
+
 }

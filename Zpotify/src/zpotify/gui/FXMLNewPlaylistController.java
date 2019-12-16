e8e0e4dbe11d885eb194881a491a9f32c8750a20
SerireS <55481182+SerireS@users.noreply.github.com>
2019-12-16 10:40:46 +0100

    clean up

--- Zpotify/src/zpotify/gui/FXMLNewPlaylistController.java
+++ Zpotify/src/zpotify/gui/FXMLNewPlaylistController.java
@@ -9,19 +9,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import zpotify.dal.DalException;
+import zpotify.gui.model.PlaylistModel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
-import zpotify.dal.DalException;
-import zpotify.gui.model.PlaylistModel;
 
 /**
  * FXML Controller class
@@ -30,7 +26,7 @@ import zpotify.gui.model.PlaylistModel;
  */
 public class FXMLNewPlaylistController implements Initializable
 {
-    
+
     private PlaylistModel playlistModel;
     private FXMLDocumentController controller;
 
@@ -49,9 +45,10 @@ public class FXMLNewPlaylistController implements Initializable
     {
         // TODO
     }
-    
+
     // Denne metode sikre vi har fat i den samme PlaylistModel og Controller hele tiden.
-    void ApplyImportantData(PlaylistModel playlistModel, FXMLDocumentController controller) {
+    void ApplyImportantData(PlaylistModel playlistModel, FXMLDocumentController controller)
+    {
         this.playlistModel = playlistModel;
         this.controller = controller;
     }
@@ -61,17 +58,17 @@ public class FXMLNewPlaylistController implements Initializable
     {
         String playlistName = txt_playlistName.getText().trim();
         Stage stage = (Stage) btn_save.getScene().getWindow();
-       // Tester om der er noget i playlistName felt, sætter en rød border for at indikere her mangles noget
-       // Det er først muligt at lukke dialogen ned når der er noget i feltet
-        if(playlistName.length() == 0)
+        // Tester om der er noget i playlistName felt, sætter en rød border for at indikere her mangles noget
+        // Det er først muligt at lukke dialogen ned når der er noget i feltet
+        if (playlistName.length() == 0)
         {
-            Border warning = new Border(new BorderStroke(Color.RED, 
-            BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));       
+            Border warning = new Border(new BorderStroke(Color.RED,
+                    BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2)));
             txt_playlistName.setBorder(warning);
         } else
         {
-        this.playlistModel.createPlaylist(playlistName);
-        stage.close();
+            this.playlistModel.createPlaylist(playlistName);
+            stage.close();
         }
     }
 
